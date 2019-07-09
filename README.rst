d335b59c9385e2be3407f2e27ac7b9c76a15814c
Peter Hawkins <27868344+pdghawk@users.noreply.github.com>
2019-07-09 12:22:14 -0400

    documentation and img examples

--- README.rst
+++ README.rst
@@ -1,7 +1,29 @@
 Adfraud
 ==========
 
-A personal project to look at ad fraud data, taken from the kaggle competition:
+A personal project to look at ad fraud data, taken from the Kaggle competition:
 
 TalkingData AdTracking Fraud Detection Challenge
 (https://www.kaggle.com/c/talkingdata-adtracking-fraud-detection)
+
+Notebooks exploring the data can be found in notebooks/. notebooks/exploratory.ipynb
+in particular contains a first look through the data.
+
+The package /adfraud contains some modules in early development for building
+ML models to study the data.
+
+see notebooks/multimodel.ipynb to see testing of different models using the
+adfraud package.
+
+Simple exploration of the data was used to quickly analyze which features were
+likely to be most important. Utilizing these findings with hash encoding and a
+random forest model, with only minimal hyperparameter optimization yields an area
+under the yield curve of 0.957 - on the test set where training and testing is
+only performed on a small subsample of all data.
+
+
+.. image :: img/example.png
+
+
+Training on the full dataset and with further optimization of the model would likely
+lead to an improved performance. 
--- adfraud/models.py
+++ adfraud/models.py
@@ -1,5 +1,7 @@
 """ Module for ML models
 """
+import numpy as np
+
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.ensemble import RandomForestClassifier
@@ -54,7 +56,7 @@ class HashForest:
             - n_splits: (optional, default=5) how many splits to perform in cross validation
         """
         cv = StratifiedShuffleSplit(n_splits=n_splits,test_size=0.2,random_state=42)
-        self.model_cv = GridSearchCV(self.model,param_grid,cv=cv,scoring='roc_auc',verbose=50)
+        self.model_cv = GridSearchCV(self.model,param_grid,cv=cv,scoring='roc_auc',verbose=10)
         self.model_cv.fit(x_train[self.feats],y_train)
         self.validated=True
 
@@ -73,16 +75,34 @@ class HashForest:
 
     @property
     def train_auc(self):
+        """ get AUC on training data """
         if self.validated:
             return self.model_cv.best_score_
         else:
             raise err.NotYetFittedModelError('You should train the model with method train_CV first \n')
 
     def plot_roc_acc(self,x_test,y_test):
+        """ plt ROC and accuracy curves on test set
+
+        Args:
+         - x_test: test data (features)
+         - y_test: test labels
+
+        """
         if self.validated:
             preds = self.model_cv.predict_proba(x_test[self.feats])
             fpr, tpr, thresholds = metrics.roc_curve(y_test, preds[:,1])
+            auc = metrics.roc_auc_score(y_test, preds[:,1])
 
+            # get accuracy of class0 and class1 predictions at different thresholds
+            acc_0 = []
+            acc_1 = []
+            for ind,th in enumerate(thresholds):
+                binary_preds = pred_from_prob(preds,th)
+                acc_1.append(accuracy_score(y_test[np.where(y_test==1)],binary_preds[np.where(y_test==1)]))
+                acc_0.append(accuracy_score(y_test[np.where(y_test==0)],binary_preds[np.where(y_test==0)]))
+
+            # set up plotting parameters
             sns.set()
             mpl.rcParams['figure.figsize']=[15.0,5.0]
             mpl.rcParams['lines.linewidth']=2.0
@@ -94,6 +114,7 @@ class HashForest:
             mpl.rcParams['axes.titleweight']='heavy'
             mpl.rcParams['legend.fontsize']=12
 
+            # build the plots- ROC in subplot 1, accuracy in subplot 2
             plt.subplot(1,2,1)
             plt.plot(fpr,tpr,'r*', label='roc auc={:.4f}'.format(auc))
             plt.plot(np.linspace(0,1,20),np.linspace(0,1,20),'k--')
@@ -122,3 +143,14 @@ class HashForest:
             return None
         else:
             raise err.NotYetFittedModelError('You should train the model with method train_CV first \n')
+
+
+def pred_from_prob(a,threshold):
+    """ turn two-class percentage prediction to binary
+    Args:
+    - a: probabilities; shape=(n_samples,2)
+    - threshold: probability above which class 1 will be counted as 1
+    """
+    bin_preds = np.zeros((np.size(a,0),))
+    bin_preds[np.where(a[:,1]>threshold)]=1.0
+    return bin_preds
